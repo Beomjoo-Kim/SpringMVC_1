219694dc96482954515db3db9358542715bf6ca5
kimbeomjoo <rebirth0116@gmail.com>
2023-03-02 17:23:22 +0900

    2023.03.02 viewResolver

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -13,6 +13,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        //springBoot 는 default 로 viewResolver 를 가지고 있는데, application.properties 에서 prefix 와 suffix 를 가져와 세팅한다.
+        return new ModelAndView("new-form");
     }
 }
